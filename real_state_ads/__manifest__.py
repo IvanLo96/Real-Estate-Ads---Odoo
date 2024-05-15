d40db7efeaa9933fd6f79e73b50bfd704ce65b6a
IvanLo96 <loveraivan013@gmail.com>
2024-05-15 19:27:41 -0400

    Primer commit en la rama main

---
+++ real_state_ads/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name":"Real Estate Ads",
+    "version":"1.0",
+    "website":"www.websitehere.com",
+    "author":"Ivan",
+    "description":"Real Estate module to show available properties",
+    "category":"Sales",
+    "depends":[],
+    "data":[],
+    "installable":True,
+    "application":True,
+    "license":"LGPL-3"
+}
---
+++ real_state_ads/models/property.py
@@ -0,0 +1,30 @@
+
+
+"""
+from odoo import fields, models
+
+
+class Property(models.Model):
+    #  models
+    _name = 'estate.property'
+    _description= 'Estate Properties'
+
+    # fields
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price")
+    best_offer = fields.Float(string="Best Offer")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area(sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden", default=False)
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection(
+        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Garden Orientation", default='north')
+
+        """
